11cef96847b4093e6d1be77258a93fd468ac7ce0
Alex Newell <122984252+AlexTNewell@users.noreply.github.com>
2023-06-22 11:40:10 +0100

    upload tests

---
+++ CRC_Terraform/end-to-end-test/index.js
@@ -0,0 +1,19 @@
+const fetch = require("node-fetch");
+
+async function foo() {
+    let obj;
+
+    const res = await fetch('https://hr1w97khtd.execute-api.us-east-1.amazonaws.com/Prod/count')
+
+    obj = await res.json();
+
+    console.log(obj);
+
+    if (obj >= 0) {
+        console.log("PASS");
+    } else {
+        throw new Error("Cannot find count value")
+    }
+}
+
+return foo();
