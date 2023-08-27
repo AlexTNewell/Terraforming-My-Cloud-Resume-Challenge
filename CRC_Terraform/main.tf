c0d293a31c8c6ad68a7b8180ea3dbe42dbfb033f
Alex Newell <122984252+AlexTNewell@users.noreply.github.com>
2023-08-27 23:46:00 +0100

    Update main.tf

--- CRC_Terraform/main.tf
+++ CRC_Terraform/main.tf
@@ -71,7 +71,7 @@ resource "aws_s3_bucket_policy" "hosting_bucket_policy" {
         "Effect" : "Allow",
         "Principal" : "*",
         "Action" : "s3:GetObject",
-        "Resource" : "arn:aws:s3:::hms-victory/*",
+        "Resource" : "arn:aws:s3:::hms-victory/*"
           }
         }
       }
